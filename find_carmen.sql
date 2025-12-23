7c5117c4fc12d852a39c219188628f11cd879f63
sarajalal2004 <sarauobeducation@gmail.com>
2025-12-23 18:01:57 +0300

    question 2

--- find_carmen.sql
+++ find_carmen.sql
@@ -9,3 +9,16 @@ SELECT code, name, region, population FROM country where population in (Select M
 
 
 
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
+-- this country's officially recognized language. Check our databases and find out what language is
+-- spoken in this country, so we can call in a translator to work with you.
+
+SELECT countrycode, language, isofficial FROM countrylanguage WHERE countrycode like 'VAT' AND isofficial = true;
+
+--  countrycode | language | isofficial
+-- -------------+----------+------------
+--  VAT         | Italian  | t
+
+
+
+
