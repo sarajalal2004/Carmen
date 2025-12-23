27002bcf12f882aef6b56af99c831f91842b2249
sarajalal2004 <sarauobeducation@gmail.com>
2025-12-23 18:04:30 +0300

    question 6

--- find_carmen.sql
+++ find_carmen.sql
@@ -56,3 +56,16 @@ SELECT distinct(city.name) As city, country.name As country, country.code FROM c
 
 
 
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
+ -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
+ -- follow right behind you!
+
+ Select city.name FROM city, country Where city.countrycode = country.code AND country.code like 'BRA' And city.id = country.capital;
+
+--     name
+-- ------------
+--  Bras∩┐╜lia
+
+
+
+
