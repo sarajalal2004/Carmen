f82c858f98fba09787b07c3a23b6a6897f7ff21a
sarajalal2004 <sarauobeducation@gmail.com>
2025-12-23 18:03:12 +0300

    question 4

--- find_carmen.sql
+++ find_carmen.sql
@@ -32,3 +32,15 @@ SELECT code, name, region, population FROM country WHERE region like 'Southern E
 --  SMR  | San Marino | Southern Europe |      27000
 
 
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
+ -- There are only two cities she could be flying to in the country. One is named the same as the country – that
+ -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
+ --  be flying to.
+
+ SELECT name FROM city where countrycode like 'SMR' AND name NOT LIKE 'San Marino';
+
+--     name
+-- ------------
+--  Serravalle
+
+
